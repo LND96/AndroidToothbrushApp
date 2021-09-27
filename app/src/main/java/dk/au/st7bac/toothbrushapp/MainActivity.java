3d5550a6e951bbffd75fb60b5eec68e70e2c9330
Line Drejer <l.n.drejer@gmail.com>
2021-09-27 10:55:02 +0200

    Test

---
+++ app/src/main/java/dk/au/st7bac/toothbrushapp/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.au.st7bac.toothbrushapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

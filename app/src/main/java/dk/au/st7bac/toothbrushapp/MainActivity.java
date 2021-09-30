b88fcc06f836e28bf2f47f4b0641b098075ce00e
Line Drejer <l.n.drejer@gmail.com>
2021-09-30 09:29:07 +0200

    Main page table

--- app/src/main/java/dk/au/st7bac/toothbrushapp/MainActivity.java
+++ app/src/main/java/dk/au/st7bac/toothbrushapp/MainActivity.java
@@ -1,16 +1,8 @@
 package dk.au.st7bac.toothbrushapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.ImageView;
@@ -18,30 +10,22 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // ui widgets
-    private TableLayout tableOverview;
     private TableRow rowHeader, rowMorning, rowEvening;
-    private TextView day1, day2, day3, day4, day5, day6, day7, txtNumberToothbrushesCompletedResult, txtTotalNumberToothbrushes, txtAvgTimeResult;
-    private ImageView morning1, morning2, morning3, morning4, morning5, morning6, morning7;
+    private TextView txtNumberToothbrushesCompletedResult, txtTotalNumberToothbrushes, txtAvgTimeResult;
 
-    private ArrayList<TextView> textViewRowHeaderList;
-    private ArrayList<ImageView> imgViewMorningList;
-    private ArrayList<String> stringRowHeaderList;
-    private String[] strings = {"Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"}; // hardcoded værdier
-
+    // data
+    private String[] headerStrings = {"Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"}; // hardcoded værdier
+    private boolean[] isToothbrushDoneMorning = {true, false, true, true, true, true, false}; // hardcoded værdier
+    private boolean[] isToothbrushDoneEvening = {false, true, true, true, true, false, true}; // hardcoded værdier
     private int toothbrushesCompleted = 10; // hardcoded værdi
     private int totalNumberToothbrushes = 14; // hardcoded værdi
     private int avgBrushTime = 45; // hardcoded værdi
@@ -52,82 +36,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // sets up user interface
         setupUI();
         startService();
     }
 
     private void setupUI() {
+        // finds UI elements
         txtNumberToothbrushesCompletedResult = findViewById(R.id.txtNumberToothbrushesCompletedResult);
         txtTotalNumberToothbrushes = findViewById(R.id.txtTotalNumberToothbrushes);
         txtAvgTimeResult = findViewById(R.id.txtAvgTimeResult);
 
-        createTable();
         updateUI();
     }
 
+    // updates UI with data
     private void updateUI() { // denne må ikke kaldes før setupUI - hvordan tager vi højde for dette? Evt. med try catch rundt om i stedet
         txtTotalNumberToothbrushes.setText(String.valueOf(totalNumberToothbrushes));
         txtNumberToothbrushesCompletedResult.setText(String.valueOf(toothbrushesCompleted));
         txtAvgTimeResult.setText(String.valueOf(avgBrushTime));
+        updateTable();
     }
 
+    // adds data to table
+    private void updateTable() {
+        rowHeader = findViewById(R.id.rowHeader);
+        rowMorning = findViewById(R.id.rowMorning);
+        rowEvening = findViewById(R.id.rowEvening);
 
-    private void createTable() {
-        tableOverview = findViewById(R.id.tableOverview);
-        rowHeader = new TableRow(this);
-        rowMorning = new TableRow(this);
-        rowEvening = new TableRow(this);
-
-        textViewRowHeaderList = new ArrayList<>();
-        textViewRowHeaderList.add(day1);
-        textViewRowHeaderList.add(day2);
-        textViewRowHeaderList.add(day3);
-        textViewRowHeaderList.add(day4);
-        textViewRowHeaderList.add(day5);
-        textViewRowHeaderList.add(day6);
-        textViewRowHeaderList.add(day7);
-
-        imgViewMorningList = new ArrayList<>();
-        imgViewMorningList.add(morning1);
-        imgViewMorningList.add(morning2);
-        imgViewMorningList.add(morning3);
-        imgViewMorningList.add(morning4);
-        imgViewMorningList.add(morning5);
-        imgViewMorningList.add(morning6);
-        imgViewMorningList.add(morning7);
-
-        stringRowHeaderList = new ArrayList<>(Arrays.asList(strings));
-
-        for (TextView textView : textViewRowHeaderList)
+        for (int i = 0; i < headerStrings.length; i++)
         {
-            textView = new TextView(this);
-            textView.setText("Test");
-            textView.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1));
-            textView.setGravity(Gravity.CENTER);
-            rowHeader.addView(textView);
+            TextView textView = new TextView(this); // create TextView
+            textView.setText(headerStrings[i]); // set text in TextView to the i'th string
+            textView.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1)); // make cells equal size
+            textView.setGravity(Gravity.CENTER); // center text
+            rowHeader.addView(textView); // add TextView to row
         }
 
-        for (int i = 0; i < 7; i++)
+        addImgToRow(rowMorning, isToothbrushDoneMorning);
+        addImgToRow(rowEvening, isToothbrushDoneEvening);
+
+    }
+
+    // adds images to table row
+    private void addImgToRow(TableRow row, boolean[] isDone)
+    {
+        for (int i = 0; i < isDone.length; i++)
         {
-            //TextView textView = new TextView(this);
-            //textView = textViewRowHeaderList.get(i);
-            textViewRowHeaderList.get(i).setText(stringRowHeaderList.get(i));
-            //textView.setText(stringRowHeaderList.get(i));
+            ImageView imageView = new ImageView(this); // create ImageView
+            imageView.setLayoutParams(new TableRow.LayoutParams(0, 80, 1)); // make cells equal size
+            imageView.setPadding(10, 10, 10, 10); // set padding around images
+            if (isDone[i])
+            {
+                imageView.setImageResource(R.drawable.tic_icon);
+            } else {
+                imageView.setImageResource(R.drawable.not_ok_icon);
+            }
+
+            row.addView(imageView);
         }
-
-        for (ImageView imageView : imgViewMorningList)
-        {
-            imageView = new ImageView(this);
-            imageView.setLayoutParams(new TableRow.LayoutParams(0, 80, 1));
-            imageView.setPadding(10, 10, 10, 10);
-            imageView.setImageResource(R.drawable.hourglass_icon);
-
-            rowMorning.addView(imageView);
-        }
-
-
-        tableOverview.addView(rowHeader);
-        tableOverview.addView(rowMorning);
     }
 
 

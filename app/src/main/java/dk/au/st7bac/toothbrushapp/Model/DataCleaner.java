db3f33e86fe7f2681560a93f6d96247e89739564
Line Drejer <l.n.drejer@gmail.com>
2021-10-11 10:06:53 +0200

    Getters and setters in TbData

---
+++ app/src/main/java/dk/au/st7bac/toothbrushapp/Model/DataCleaner.java
@@ -0,0 +1,16 @@
+package dk.au.st7bac.toothbrushapp.Model;
+
+import java.util.ArrayList;
+
+public class DataCleaner {
+    
+    private ArrayList<TbData> tbCleanDataList; // skal den initieres et sted?
+    
+    public ArrayList<TbData> CleanData(ArrayList<TbData> tbDataList) {
+
+        for (TbData tbData : tbDataList) {
+        }
+        
+        return tbCleanDataList;
+    }
+}
--- app/src/main/java/dk/au/st7bac/toothbrushapp/Model/TbData.java
+++ app/src/main/java/dk/au/st7bac/toothbrushapp/Model/TbData.java
@@ -7,6 +7,8 @@ import com.google.gson.annotations.SerializedName;
 
 public class TbData {
 
+    // Der er lavet public gettere og settere for alle parametre, men overvej om set skal være private!
+
     private String sysId;
     private String dateTime;
     private int tbVal;
@@ -16,6 +18,38 @@ public class TbData {
         this.sysId = sysId;
         this.dateTime = dateTime;
         this.tbVal = tbVal;
+        this.tbSecs = tbSecs;
+    }
+
+    public String getSysId() {
+        return sysId;
+    }
+
+    public void setSysId(String sysId) {
+        this.sysId = sysId;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getTbVal() {
+        return tbVal;
+    }
+
+    public void setTbVal(int tbVal) {
+        this.tbVal = tbVal;
+    }
+
+    public double getTbSecs() {
+        return tbSecs;
+    }
+
+    public void setTbSecs(double tbSecs) {
         this.tbSecs = tbSecs;
     }
 
